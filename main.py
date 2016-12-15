c898671998e4df59af94f243426e23916fe53f17
henry921630@gmail.com <henry921630@gmail.com>
2016-12-15 18:11:07 +0800

    test

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ def handle(msg):
 
 
                 if len(accrecord)<4 or len(accrecord)>5 or len(accDate)<>10: #如果格式不太合
-                    BBMresponse_str1=salutation+ " 你的記帳格式不對唷！記得空格要空對！ \n給你一個範例：「嗶鼠記帳 20161116 生日大餐 $999」"                    
+                    BBMresponse_str1=salutation+ " 你的記帳格式不對唷！記得空格要空對！ \n給你一個範例：「嗶鼠記帳 20161116 生日大餐 $999」" + accDate                    
                 else:
                     bot.sendMessage(chat_id,"等我一下，我來翻找一下我的記帳小本子")
 
