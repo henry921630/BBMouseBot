6e940d90cb9875ca03fcec6418bed126f09d685d
henry921630@gmail.com <henry921630@gmail.com>
2016-12-15 18:17:20 +0800

    BF

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ testingtoken='290645324:AAGBYFAnK6yCusuijM3plvDfhnxk3rgIlsg'
 version="v12151748"
 B=bbmousetoken
 T=testingtoken
-mode=B
+mode=T
 
 
 def auth_gss_client(path, scopes):
@@ -197,8 +197,8 @@ def handle(msg):
 
 
 
-                if len(accrecord)<4 or len(accrecord)>5 or len(accDate)<>10: #如果格式不太合
-                    BBMresponse_str1=salutation+ " 你的記帳格式不對唷！記得空格要空對！ \n給你一個範例：「嗶鼠記帳 20161116 生日大餐 $999」" + accDate                    
+                if len(accrecord)<4 or len(accrecord)>5 or len(accrecord[orderofDate])<>10: #如果格式不太合
+                    BBMresponse_str1=salutation+ " 你的記帳格式不對唷！記得空格要空對！ \n給你一個範例：「嗶鼠記帳 20161116 生日大餐 $999」"                  
                 else:
                     bot.sendMessage(chat_id,"等我一下，我來翻找一下我的記帳小本子")
 
