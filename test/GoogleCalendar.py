6c9161e47906db773167ec5033e4a38587bb3c88
henry921630@gmail.com <henry921630@gmail.com>
2017-01-12 11:13:04 +0800

    時間別名

--- test/GoogleCalendar.py
+++ test/GoogleCalendar.py
@@ -29,6 +29,7 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
+
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
